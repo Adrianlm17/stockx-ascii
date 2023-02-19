b5166249febe45c3b87c93edab3dfe9598bcd746
Adrián López <adrianlomu2004@gmail.com>
2023-02-19 15:55:57 +0100

    featureShowInfo(AndCriteria | Size): implementar historia usuario filtro Size/Sales.

---
+++ stocx/src/main/java/edu/craptocraft/stockx/criteria/AndCriteria.java
@@ -0,0 +1,32 @@
+package edu.craptocraft.stockx.criteria;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.craptocraft.stockx.item.Item;
+import edu.craptocraft.stockx.item.Offer;
+
+public class AndCriteria implements Criteria{
+
+    private Criteria criteria = null;
+    private Criteria otherCriteria = null;
+
+    public AndCriteria(Criteria criteria, Criteria otherCriteria) {
+        this.criteria = criteria;
+        this.otherCriteria = otherCriteria;
+    }
+    
+    @Override
+    public List<Offer> checkCriteria(Item sneaker) {
+        List<Offer> firstCriteriaItems = criteria.checkCriteria(sneaker);
+        List<Offer> otherCriteriaItems = otherCriteria.checkCriteria(sneaker);
+
+        List<Offer> items = new ArrayList<Offer>();
+        for (Offer offer : firstCriteriaItems) {
+            if (otherCriteriaItems.contains(offer)) {
+                items.add(offer);
+            }
+        }
+        return items;
+    }
+}
